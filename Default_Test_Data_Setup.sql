9da4548212463326bff25159bfd1fcd92e4e1112
10393759 <37912906+10393759@users.noreply.github.com>
2018-09-23 19:27:28 +0100

    Add files via upload

---
+++ Default_Test_Data_Setup.sql
@@ -0,0 +1,5 @@
+#Setup Default Test Data
+INSERT INTO bookings (seatName) values ("Row A aisle");
+INSERT INTO bookings (seatName) values ("Row A window");
+INSERT INTO bookings (seatName) values ("Row B aisle");
+INSERT INTO bookings (seatName) values ("Row B window");
